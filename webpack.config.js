4a2cbed836efe5e3efd90266b9e40509de06278c
Andrew Talle <talle.andrew@gmail.com>
2022-03-01 09:41:06 -0700

    update webpack and create server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
@@ -6,12 +7,34 @@ module.exports = {
   output: {
     filename: "bundle.[hash].js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
   },
+  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
+  devServer: {
+    port: 8080,
+    hot: true,
+    static: {
+      // match the output path
+      directory: path.resolve(__dirname, 'dist'),
+      //match the output 'publicPath'
+      publicPath: '/'
+    },
+    headers: { 'Access-Control-Allow-Origin': '*'},
+
+    // proxy is required in order to make api calls to express server while using hot-reload webpack server
+    // routes api fetch requests from localhost:8080/api/* (webpack dev server) to localhost:3000/api/* (where our Express server is running)
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000',
+        secure: false,
+      }, 
+    },
+  },
   resolve: {
     modules: [__dirname, "src", "node_modules"],
     extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
